e478dc39c6f4ab24e0369dfa20716fec93293033
JINIL8 <sonijinil9841@gmail.com>
2019-07-04 18:55:22 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { User } from './user';
+import { EnrollmentService } from './enrollment.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tdf';
+  topics   = ['Angular', 'React', 'Vue'];
+  topicsHashError = true;
+
+  userModel = new User('', 'jinil@gmail.com', 1234567890, '', 'morning', true);
+
+  constructor( private enrollmentService: EnrollmentService) {}
+  validateTopic(value) {
+      if (value === 'default') {
+        this.topicsHashError = true;
+      } else {
+        this.topicsHashError = false;
+      }
+  }
+  onSubmit() {
+      this.enrollmentService.enroll(this.userModel)
+     .subscribe (
+        data => console.log ('Success!', data),
+        error => console.log('Error', error)
+      )
+    }
 }
